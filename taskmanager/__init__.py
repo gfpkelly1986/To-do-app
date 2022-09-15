8ba24f2e7d6cedfa017b3875738bd55fefb01d45
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-15 18:10:32 +0000

    Set up a basic Flask app with SQLAlchemy

---
+++ taskmanager/__init__.py
@@ -0,0 +1,23 @@
+# importing the os allows you to interact with the
+# operating system on the computer.
+import os # noqa
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# env.py not visible in github, check filepath exits locally:
+# Cannot just impot it directly
+if os.path.exists("env.py"):
+    import env # noqa
+
+# Create an instance of the Flask object
+app = Flask(__name__)
+# Specify two app configuration variables
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# set the database location
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Create an instance of the SQLAlchemy class,
+# Store in db variable, set to instance of our Flask app
+# You are binding the instance of SQLAlchemy to the application called taskmanager
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
