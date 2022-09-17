f20e70142adcdebca1c7effaaa43ca480a126b15
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-17 12:11:13 +0000

    Add Delete functionality to application

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav)
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let deleteModal = document.querySelectorAll('.modal');
+    M.Modal.init(deleteModal);
+  });
