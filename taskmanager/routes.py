697eea13447d67fb556d10f6f4a7167fbe705dd4
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-17 10:12:29 +0000

    Add read functionality to application

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,9 +10,10 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
-
+# This is the C in CRUD
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
