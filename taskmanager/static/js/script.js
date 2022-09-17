404afad87ec91380b2790743127e306faadd1703
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-17 14:55:42 +0000

    Add tasks to the front end

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav)
+document.addEventListener('DOMContentLoaded', function () {
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav)
+
+  let deleteModal = document.querySelectorAll('.modal');
+  M.Modal.init(deleteModal);
+
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let deleteModal = document.querySelectorAll('.modal');
-    M.Modal.init(deleteModal);
-  });
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+
+let collapsibles = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsibles);
